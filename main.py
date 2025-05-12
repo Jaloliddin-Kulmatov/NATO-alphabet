01d7da1d8e03f0b9d2ae5d41e32198a8136e87af
jaloliddinqulmatov <jaloliddinqulmatov12@gmail.com>
2025-05-13 00:05:55 +0900

    update

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pandas
+
 alphabet = (pandas.read_csv("nato_phonetic_alphabet.csv"))
 
 new_alphabet = {row.letter: row.code for (index, row) in alphabet.iterrows()}
+def generate_nato_alphabet():
+    word_input = input("Write a word: ").upper()
+    try:
+        answer_words = [new_alphabet[letter] for letter in word_input]
+    except KeyError:
+        print("Sorry only letter in alphabet please")
+        generate_nato_alphabet()
+    else:
+        print(answer_words)
+generate_nato_alphabet()
 
-word_input = input("Write a word: ").upper()
 
-answer_words = [new_alphabet[letter] for letter in word_input]
 
-print(answer_words)
+
